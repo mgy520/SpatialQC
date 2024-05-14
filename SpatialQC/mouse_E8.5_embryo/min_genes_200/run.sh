01422b4eb178b15ee5f3245c71d0be00e7fee32c
mgy520 <82570225+mgy520@users.noreply.github.com>
2024-05-15 00:52:44 +0800

    Update run.sh

--- SpatialQC/mouse_E8.5_embryo/min_genes_200/run.sh
+++ SpatialQC/mouse_E8.5_embryo/min_genes_200/run.sh
@@ -1,1 +1,1 @@
- python -m SpatialQC --adata E8.5-Atlas_run_min200.h5ad --markers E8.5_markers.csv --slice slice --mito 'mt-' --ribo 'Rps,Rpl' --min_cells_list 3 10 20 --mito_percent 0.2 --min_genes_list2 0 100 200 300 400 500 600 --min_cells_list 10 15 20 --doublet False --l 1
+ python -m SpatialQC --input E8.5-Atlas_run_min200.h5ad --markers E8.5_markers.csv --slice slice --mito 'mt-' --ribo 'Rps,Rpl' --min_cells_list 3 10 20 --mito_percent 0.2 --min_genes_list2 0 100 200 300 400 500 600 --min_cells_list 10 15 20 --doublet False --l 1
